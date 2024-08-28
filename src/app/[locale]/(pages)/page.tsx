22e09dfb220fcc7602f24611414cf04ba928cb2a
theebestzz <f.eminn18@gmail.com>
2024-08-29 01:39:48 +0300

    bad choice for design, globe component

--- src/app/[locale]/(pages)/page.tsx
+++ src/app/[locale]/(pages)/page.tsx
@@ -4,9 +4,9 @@ import { WorldSection } from "@/components/ui/world";
 export default function HomePage() {
   return (
     <div className="relative -z-50 flex min-h-[75vh] w-full items-center justify-center">
-      <div className="absolute bottom-0 left-0 right-0 flex w-full items-center justify-center">
+      {/* <div className="absolute bottom-0 left-0 right-0 flex w-full items-center justify-center">
         <WorldSection />
-      </div>
+      </div> */}
       <HomeBlock />
     </div>
   );
--- src/app/_components/site/blocks/home-block.tsx
+++ src/app/_components/site/blocks/home-block.tsx
@@ -8,13 +8,13 @@ export function HomeBlock() {
   return (
     <div className="relative flex w-full flex-col items-center justify-center rounded-md antialiased">
       <motion.div
-        // initial={{ opacity: 0.0, y: 40 }}
-        // whileInView={{ opacity: 1, y: 0 }}
-        // transition={{
-        //   delay: 0.3,
-        //   duration: 0.8,
-        //   ease: "easeInOut",
-        // }}
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
         className="mx-auto max-w-2xl space-y-3 p-4"
       >
         <h1 className="relative z-10 bg-gradient-to-r from-pink-300 via-purple-400 to-violet-600 bg-clip-text text-center text-4xl font-bold text-transparent dark:from-pink-200 dark:via-purple-400 md:text-7xl">
